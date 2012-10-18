ea78ed1a1b823f7303723b74c7a0a64a0b4bad28
Brett Randall <javabrett@gmail.com>
2012-10-18 21:35:06 +1100

    Added test for GroupedTimingStatistics with setCreateRollupStatistics(true). Cleaned-up a couple of test warnings.

--- src/test/java/org/perf4j/GroupedTimingStatisticsTest.java
+++ src/test/java/org/perf4j/GroupedTimingStatisticsTest.java
@@ -64,8 +64,59 @@ public class GroupedTimingStatisticsTest extends TestCase {
         assertOutputContains(groupStatistics.toString(), "a           1.1           4           3         0.2           5           6");
     }
 
+    private static class TestStopWatch extends StopWatch {
 
+        private static final long serialVersionUID = 1L;
 
+        private String tag;
+
+        private long elapsedTime;
+
+        @Override
+        public String getTag() {
+            return tag;
+        }
+
+        @Override
+        public StopWatch setTag(String tag) {
+            this.tag = tag;
+            return this;
+        }
+
+        @Override
+        public long getElapsedTime() {
+            return elapsedTime;
+        }
+
+        public void setElapsedTime(long elapsedTime) {
+            this.elapsedTime = elapsedTime;
+        }
+    }
+
+    public void testCreateRollupStatistics() throws Exception {
+
+        GroupedTimingStatistics groupStatistics = new GroupedTimingStatistics();
+        groupStatistics.setCreateRollupStatistics(true);
+
+        TestStopWatch s = new TestStopWatch();
+        s.setTag("a.1");
+        s.setElapsedTime(10);
+        groupStatistics.addStopWatch(s);
+
+        s.setTag("a.2");
+        s.setElapsedTime(20);
+        groupStatistics.addStopWatch(s);
+
+        s.setTag("a");
+        s.setElapsedTime(20);
+        groupStatistics.addStopWatch(s);
+
+        System.out.println(groupStatistics.toString());
+        assertOutputContains(groupStatistics.toString(), "Tag     Avg(ms)         Min         Max     Std-Dev       Count       Total");
+        assertOutputContains(groupStatistics.toString(), "a          16.7          10          20         4.7           3          50");
+        assertOutputContains(groupStatistics.toString(), "a.1        10.0          10          10         0.0           1          10");
+        assertOutputContains(groupStatistics.toString(), "a.2        20.0          20          20         0.0           1          20");
+    }
 
     private void assertOutputContains(String output, String expectedToContain) {
         String message = "Expected toString() output to contain the given string, matching formatting.\n" + expectedToContain +
